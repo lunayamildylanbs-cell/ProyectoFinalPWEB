eb7c164c41465831b1eff3c93c71d38d6334d6b0
lunayamildylanbs-cell <luna.yamildylanbs@gmail.com>
2025-12-11 17:01:14 -0400

    Se elimino la funcion del pdf ya que no se utilizara

--- Archivos JS/global.js
+++ Archivos JS/global.js
@@ -6,20 +6,3 @@ hamburguesa.addEventListener("click", () => {
   // alternar si el menu se muestra o se oculta
   nav.style.display = nav.style.display === "flex" ? "none" : "flex";
 });
-
-
-// Abrir manual 
-document.addEventListener("DOMContentLoaded", function () {
-    const pdfLinks = document.querySelectorAll(".pdf-link");
-    const pdfContainer = document.getElementById("pdf-container");
-    const pdfFrame = document.getElementById("pdf-frame");
-
-    pdfLinks.forEach(link => {
-        link.addEventListener("click", function (e) {
-            e.preventDefault(); 
-            const pdfPath = this.getAttribute("data-pdf");
-            pdfFrame.src = pdfPath;
-            pdfContainer.style.display = "block";
-        });
-    });
-});
